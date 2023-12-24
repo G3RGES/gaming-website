08ca46905d8f31b5115d834d33227b3a8ef72971
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-24 14:18:17 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,77 @@ import React from "react";
 // logo
 import Logo from "../assets/logo.png";
 
+// react-icons
+import { IoSearch } from "react-icons/io5";
+
 const Navbar = () => {
   return (
     <>
       <div className="bg-primary">
         <div className="container">
           <div className="flex justify-between items-center">
-            <div className="">
+            <div className="flex gap-2 items-center">
               <a href="" className="">
-                <img className="w-30" src={Logo} alt="logo" />
+                <img className="w-16" src={Logo} alt="logo" />
               </a>
+              {/* //* made to create a seperator */}
+              <div className="border-r-2 border-tertiary text-transparent ">
+                {"|||"}
+              </div>
+
+              <div className="text-white flex ">
+                <ul className="flex gap-5 items-center">
+                  <li className="">
+                    <a className="" href="#/explore">
+                      Explore
+                    </a>
+                  </li>
+                  <li className="">
+                    <a className="" href="#/release">
+                      Release
+                    </a>
+                  </li>
+                  <li className="relative">
+                    <input
+                      className="bg-gray-700/50 pl-5 py-2 px-4 rounded-2xl 
+                      outline-none"
+                      type="search"
+                      name="search"
+                      id="search"
+                      placeholder="Search for games..."
+                    />
+                    <IoSearch
+                      className="text-gray-500 absolute 
+                    right-3 top-1/2 -translate-y-1/2  h-6 w-9"
+                    />
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className=""></div>
+
+            <div className="text-white">
+              <ul className="flex gap-4 items-center ">
+                <li className="">
+                  <a
+                    href="#about"
+                    className="bg-gray-400/20  shadow-md font-semibold py-2 px-4
+                     rounded-2xl inline-block"
+                  >
+                    Log In
+                  </a>
+                </li>
+                <li className="">
+                  <a
+                    href="#about"
+                    className="bg-gradient-to-r 
+                     from-purple-800 to-secondary shadow-md font-semibold py-2 px-4
+                     rounded-2xl inline-block"
+                  >
+                    Sign Up
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
