d77fea5c02f7eb03894702df44a2c6eafb42447a
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-24 13:00:58 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,26 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 
+// logo
+import Logo from "../assets/logo.png";
+
 const Navbar = () => {
-  return <div className="">Navbar</div>;
+  return (
+    <>
+      <div className="bg-primary">
+        <div className="container">
+          <div className="flex justify-between items-center">
+            <div className="">
+              <a href="" className="">
+                <img className="w-30" src={Logo} alt="logo" />
+              </a>
+            </div>
+            <div className=""></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
