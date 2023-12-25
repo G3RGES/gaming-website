6208a5727da5b823d663a69ec4d736a03f2e9088
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-25 08:45:23 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
+// image
+import CoverImg from "../assets/hero/hero_cover.jpg";
+import HeroImg from "../assets/hero/hero_png.png";
+
+const bgImage = {
+  backgroundImage: `url(${CoverImg})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  height: "100%",
+  width: "100%",
+};
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <div className="" style={bgImage}>
+        <div className="container">
+          {/* content */}
+          <div className=""></div>
+
+          <div className="">
+            <img className="" src={HeroImg} alt="hero_img" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
