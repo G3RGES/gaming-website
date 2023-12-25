988348c46ff62e817819bc9ca6c50a9b45f396ea
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-25 11:14:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TrendingGames from "./components/TrendingGames";
+import RecomendedGames from "./components/RecomendedGames";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <TrendingGames />
+        <RecomendedGames />
       </div>
     </>
   );
