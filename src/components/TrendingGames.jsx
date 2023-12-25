991e61e1c01718a2c1b13b9404a26c13bcce72b9
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-25 10:34:47 +0200

    Update TrendingGames.jsx

--- src/components/TrendingGames.jsx
+++ src/components/TrendingGames.jsx
@@ -3,7 +3,19 @@ import React from "react";
 const TrendingGames = () => {
   return (
     <>
-      <div className="container"></div>
+      <section className="py-10 bg-primary text-white">
+        <div className="container">
+          <div className="flex flex-row justify-between items-center px-3">
+            <h1 className=" font-bold text-2xl">Currently Trending Games</h1>
+            <button
+              className="bg-gray-400/50 text-white
+             rounded-xl py-3 px-4 font-semibold"
+            >
+              View All
+            </button>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
