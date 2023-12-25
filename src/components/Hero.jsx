3f81252b879d7223054137f5a21842d7c5242256
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-25 09:19:20 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,9 @@ import React from "react";
 import CoverImg from "../assets/hero/hero_cover.jpg";
 import HeroImg from "../assets/hero/hero_png.png";
 
+// icons
+import { FaArrowRight } from "react-icons/fa6";
+
 const bgImage = {
   backgroundImage: `url(${CoverImg})`,
   backgroundSize: "cover",
@@ -16,13 +19,34 @@ const bgImage = {
 const Hero = () => {
   return (
     <>
-      <div className="" style={bgImage}>
-        <div className="container">
-          {/* content */}
-          <div className=""></div>
+      <div className="min-h-[550px] flex  items-center" style={bgImage}>
+        <div className="bg-primary/60 min-h-[550px] w-screen flex  items-center">
+          <div className="container ">
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
+              {/* content */}
+              <div className="flex flex-col justify-center items-start">
+                <p className="text-lg text-purple-500 font-semibold">
+                  Discover Games You Will Love
+                </p>
+                <h1 className="text-white font-semibold text-6xl">
+                  Read About Games You Love
+                </h1>
+                <button
+                  className="mt-8 bg-gradient-to-r
+                from-purple-800 to-tertiary rounded-2xl 
+                py-2 px-4 text-white font-semibold flex 
+                flex-row items-center justify-center gap-3 text-lg"
+                >
+                  Start Exploring
+                  <FaArrowRight className="hover:translate-x-1 duration-300 w-8" />
+                </button>
+              </div>
 
-          <div className="">
-            <img className="" src={HeroImg} alt="hero_img" />
+              {/* image  */}
+              <div className="">
+                <img className="" src={HeroImg} alt="hero_img" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
