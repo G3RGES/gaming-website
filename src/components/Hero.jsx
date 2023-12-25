fa6785c3e3e583d642192b6c1a37c595c380ec7d
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-25 10:10:42 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
   return (
     <>
       <div className="min-h-[550px] flex  items-center" style={bgImage}>
-        <div className="bg-primary/60 min-h-[550px] w-screen flex  items-center">
+        <div className="bg-primary/60 min-h-[550px] w-screen flex items-center">
           <div className="container ">
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
               {/* content */}
