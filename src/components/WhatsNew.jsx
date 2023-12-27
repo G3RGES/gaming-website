0c57aaa28ee33cd938b93fa2cf72f25527649566
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-27 04:16:03 +0200

    Update WhatsNew.jsx

--- src/components/WhatsNew.jsx
+++ src/components/WhatsNew.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const WhatsNew = () => {
-  return <div>WhatsNew</div>;
+  return (
+    <>
+      <div className="py-10 bg-primary text-white">
+        <div className="container ">
+          {/* Header Section */}
+          <div className="flex flex-row justify-between items-center px-3">
+            <h1 className=" font-bold text-2xl">What&apos;s New</h1>
+            <button
+              className="bg-gray-400/50 text-white
+            rounded-xl py-3 px-4 font-semibold z-10"
+            >
+              View All
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default WhatsNew;
