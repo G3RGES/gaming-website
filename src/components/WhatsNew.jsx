436af92aee97cbe57597bf83e23abf38d917facc
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-28 02:43:10 +0200

    Update WhatsNew.jsx

--- src/components/WhatsNew.jsx
+++ src/components/WhatsNew.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 
 const WhatsNew = () => {
@@ -7,12 +8,12 @@ const WhatsNew = () => {
         <div className="container ">
           {/* Header Section */}
           <div className="flex flex-row justify-between items-center px-3">
-            <h1 className=" font-bold text-2xl">What&apos;s New</h1>
+            <h1 className=" font-bold text-2xl">What's New</h1>
             <button
               className="bg-gray-400/50 text-white
             rounded-xl py-3 px-4 font-semibold z-10"
             >
-              View All
+              Explore
             </button>
           </div>
         </div>
