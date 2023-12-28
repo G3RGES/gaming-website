a36fa72930bdc63e194be6ba21f12293930a58db
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-28 04:42:27 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import Hero from "./components/Hero";
 import TrendingGames from "./components/TrendingGames";
 import RecomendedGames from "./components/RecomendedGames";
 import WhatsNew from "./components/WhatsNew";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
-      <div className="">
+      <div className="overflow-hidden">
         <Navbar />
         <Hero />
         <TrendingGames />
         <RecomendedGames />
         <WhatsNew />
+        <Footer />
       </div>
     </>
   );
