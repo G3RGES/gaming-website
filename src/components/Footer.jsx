835cbf01218ef73c0932a187936b69231844b886
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-28 04:42:22 +0200

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="bg-primary text-white py-10">
+        <div className="container text-center">
+          <h1 className="text-3xl font-bold">Let's Play a Game</h1>
+          <hr className="w-[150px] mx-auto my-4" />
+          <p className="text-white/70">Made with ❤️ by G3RGES</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
